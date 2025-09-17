5f9f0c990ea8b2a2283dc8316c39c9664f101a8d
Hugo LIEGEARD <hugo@hl-media.co>
2025-09-17 12:19:36 +0200

    Travail de la matinée du  17/09/25
    - Revue des éléments de la veille
    - Découverte de Twig
    - Création d'un template "base.html.twig"
    - Utilisation des notions de block, include
    - Création des pages Accueil et Categorie
    - Mise en pratique autour de la page article.
    - Découverte de la notion de paramètre de route dans Symfony.

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,16 +3,18 @@
 namespace App\Controller;
 
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class DefaultController
+class DefaultController extends AbstractController
 {
     #[Route('/', name: 'default_home', methods: ['GET'])]
     public function home(): Response
     {
-        return new Response("<html><body><h1>Hello World!</h1></body></html>");
+        # return new Response("<html><body><h1>Hello World!</h1></body></html>");
+        return $this->render('default/home.html.twig');
     }
 
     #[Route('/accueil', name: 'default_accueil', methods: ['GET'])]
@@ -20,6 +22,16 @@ class DefaultController
     {
         return new RedirectResponse('/');
     }
+
+    # -- Routes / Pages pour mes catégories
+
+    #[Route('/{aliasDeMaCategorie}', name: 'default_category', methods: ['GET'])]
+    public function category($aliasDeMaCategorie = null): Response
+    {
+        return $this->render('default/category.html.twig');
+    }
+
+
 }
 
 
